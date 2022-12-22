2e8c2635502a8673e403194f540ae161237b8dcd
Proxsy03 <you@example.com>
2022-12-22 09:55:31 +0100

    commit backup avant environment / contient les door animations

---
+++ Assets/ViewOrient.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewOrient : MonoBehaviour
+{
+    private float x;
+    private float y;
+    public float sensitivity = -1f;
+    private Vector3 rotate;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Debug.Log(transform.position.x);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        y = Input.GetAxis("Mouse X");
+        x = Input.GetAxis("Mouse Y");
+        rotate = new Vector3(x, y * sensitivity, 0);
+        transform.eulerAngles = transform.eulerAngles - rotate; 
+    }
+}
